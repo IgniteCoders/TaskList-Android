4c86774d29842455e7fdf31a4732e9ae6f1817b2
Mansour <theboom.kajas@gmail.com>
2026-05-04 19:33:00 +0200

    Added swipe gestures to the task list.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/tasklist/activities/TaskListActivity.kt
+++ app/src/main/java/com/example/tasklist/activities/TaskListActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tasklist.activities
 
 import android.content.Intent
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tasklist.R
 import com.example.tasklist.adapters.TaskAdapter
 import com.example.tasklist.data.Category
@@ -15,6 +18,8 @@ import com.example.tasklist.data.Task
 import com.example.tasklist.data.TaskDAO
 import com.example.tasklist.databinding.ActivityTaskListBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
+import java.util.Locale
 
 class TaskListActivity : AppCompatActivity() {
 
@@ -63,6 +68,8 @@ class TaskListActivity : AppCompatActivity() {
             intent.putExtra(TaskDetailActivity.EXTRA_CATEGORY_ID, category?.id ?: -1)
             startActivity(intent)
         }
+
+        configureGestures()
     }
 
     override fun onResume() {
@@ -104,6 +111,8 @@ class TaskListActivity : AppCompatActivity() {
         intent.putExtra(TaskDetailActivity.EXTRA_CATEGORY_ID, task.category.id)
         intent.putExtra(TaskDetailActivity.EXTRA_TASK_ID, task.id)
         startActivity(intent)
+
+        adapter.notifyItemChanged(position)
     }
 
     fun deleteTask(position: Int) {
@@ -118,7 +127,7 @@ class TaskListActivity : AppCompatActivity() {
                 reloadData()
             }
             .setNegativeButton("Cancelar") { dialog, which ->
-
+                adapter.notifyItemChanged(position)
             }
             //.setCancelable(false)
             .create()
@@ -126,5 +135,59 @@ class TaskListActivity : AppCompatActivity() {
         dialog.show()
     }
 
+    fun configureGestures() {
+        val gestures = ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    adapter.notifyItemMoved(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
+                    return true
+                }
 
+                override fun onSwiped(
+                    viewHolder: RecyclerView.ViewHolder,
+                    direction: Int
+                ) {
+                    if (direction == ItemTouchHelper.LEFT) {
+                        deleteTask(viewHolder.absoluteAdapterPosition)
+                    } else {
+                        editTask(viewHolder.absoluteAdapterPosition)
+                    }
+                }
+
+                override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder,
+                                         dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+
+                    val whiteColor = getColor(R.color.white)
+
+                    RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+
+                        // Swipe left action
+                        .addSwipeLeftLabel("BORRAR")
+                        .setSwipeLeftLabelColor(whiteColor)
+                        .addSwipeLeftActionIcon(R.drawable.ic_delete)
+                        .setSwipeLeftActionIconTint(whiteColor)
+                        .addSwipeLeftBackgroundColor(getColor(R.color.negative))
+
+                        // Swipe right action
+                        .addSwipeRightLabel("EDITAR")
+                        .setSwipeRightLabelColor(whiteColor)
+                        .addSwipeRightActionIcon(R.drawable.ic_edit)
+                        .setSwipeRightActionIconTint(whiteColor)
+                        .addSwipeRightBackgroundColor(getColor(R.color.black))
+
+                        // Build
+                        .create()
+                        .decorate()
+
+                    super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                }
+
+            }
+        )
+        gestures.attachToRecyclerView(binding.recyclerView)
+    }
 }
